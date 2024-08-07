57b1c21ae35cc42d97b62016152608389de5c038
nitesw <apedos2007@gmail.com>
2024-08-08 00:01:47 +0300

    Add project files.

---
+++ TestTask_MvcApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TestTask_MvcApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
